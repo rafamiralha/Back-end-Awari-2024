0682dbccbca8c64d46f098e6ef98b10fc5248c0f
Rafael Miralha <rafamiralha1@gmail.com>
2024-05-07 20:21:51 -0300

    update in index.js

--- API_Products/app/index.js
+++ API_Products/app/index.js
@@ -3,10 +3,14 @@ import {create, read, update, remove} from './services/product-services.js';
 const server = express();
 server.use(express.json());
 server.listen(3000);
-
+//baseUrl localhost:3000
+//path /api/products
+//query param: ?Produto=Iphone&description=S
 
 server.get('/api/products', (request,response)=>{
-    read();
+    const {name,description,price,category} = request.query;
+    const filter = {name,description,price,category};
+    read(filter);
     return response.status(200).json({message: "{GET} Deu certo!"});
 });
 
@@ -33,13 +37,32 @@ server.post('/api/products', (request,response)=>{
 });
 
 
-server.put('/api/products', (request,response)=>{
-    console.log('Put funcionando');
+server.put('/api/products/:id', (request,response)=>{
+    const {
+        name,
+        shortDescription,
+        description,
+        price,
+        category,
+        urlBanner
+    } = request.body;
+    const payload = { 
+        name,
+        shortDescription,
+        description,
+        price,
+        category,
+        urlBanner
+    };
+    const { id } =  request.params;
+    update(payload,id)
     return response.status(200).json({message: "{PUT} Deu certo!"});
 });
 
 
-server.delete('/api/products', (request,response)=>{
+server.delete('/api/products/:id', (request,response)=>{
+    const { id } = request.params;
+    remove({id});
     console.log('Delete funcionando');
-    return response.status(200).json({message: "{DELETE} Deu certo!"});
+    return response.status(204).json({message: "{DELETE} Deu certo!"});
 });
